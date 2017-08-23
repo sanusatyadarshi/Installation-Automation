2938f6e1ee9e416d5f44ce78a0eae580124a4743
Sanu Satyadarshi <sanusatyadarshi@users.noreply.github.com>
2017-08-23 22:35:18 +0530

    Add files via upload

---
+++ automation.bat
@@ -0,0 +1,28 @@
+@echo off
+title "Automating Default HTTPS Integration for Stealth Web Services"
+REM this is an automated batch script for Default HTTPS Integration for Stealth Web Services
+
+:: Installs	OPENSSL for Windows   (https://wiki.openssl.org/index.php/Binaries)
+C:\Users\60d0f10\Downloads\Programs\Win64OpenSSL-1_1_0f.exe /silent 
+::wscript "sendkey.vbs"
+setx path "%PATH%;C:\OpenSSL\bin" 
+echo OPENSSL installation completed:Step 1
+pause 
+
+::Generate Private Key and Cert
+ ::runas /user:AP\satyads cmd
+
+
+::cd "c:\Windows\System32\"
+start cmd  
+
+::start secondbatchscript.bat
+
+::cd"c:\Program Files"
+::start openssl
+
+::pkcs12 -in ustr-erl-8792-06-27-2016.pfx -out tempcertfile.crt -nodes
+
+exit
+
+  
